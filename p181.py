d384542f1772551612708166cc42dc7854f19514
shenxj <2200319548@qq.com>
2018-06-10 10:17:04 -0700

    update

--- p181.py
+++ p181.py
@@ -14,8 +14,9 @@ with tf.Session() as sess:
 	img_data = tf.image.resize_images(img_data,[180,267],method=1)
 	batched = tf.expand_dims(tf.image.convert_image_dtype(img_data,tf.float32),0)
 	boxes = tf.constant([[[0.05,0.05,0.9,0.7],[0.35,0.47,0.5,0.56]]])
-	result = tf.image.draw_bounding_boxes(img_data,boxes)
+	result = tf.image.draw_bounding_boxes(batched,boxes)
 	
+	result = tf.squeeze(result,0)
 	print result.eval()
 	plt.imshow(result.eval())
 	plt.show()
