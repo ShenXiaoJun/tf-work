9424105ce2f3d6f9efd163c9cc540a2db51e961e
shenxj <2200319548@qq.com>
2018-07-07 20:30:08 +0800

    update

--- p41-2.py
+++ p41-2.py
@@ -1,10 +1,29 @@
 #encoding:utf-8
 import tensorflow as tf
 
+import time
+import datetime
+
+
 a = tf.constant([1.0, 2.0], name="a")
 b = tf.constant([2.0, 3.0], name="b")
 
-g = tf.Graph
+g = tf.Graph()
 #指定计算运行的设备
-with g.device('/device:GPU:0'):
+with g.device('/gpu:0'):
+    start = datetime.datetime.now()
     result = a + b
+    end = datetime.datetime.now()
+    print (end - start).microseconds
+
+with g.device('/cpu'):
+    start = datetime.datetime.now()
+    result = a + b
+    end = datetime.datetime.now()
+    print (end - start).microseconds
+
+with g.device('/cpu:0'):
+    start = datetime.datetime.now()
+    result = a + b
+    end = datetime.datetime.now()
+    print (end - start).microseconds
