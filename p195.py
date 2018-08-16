b2b6e494810c5c1e0e9dbcda28fbc0f6b3ef9703
shenxj <2200319548@qq.com>
2018-08-16 12:51:41 +0800

    update

--- p195.py
+++ p195.py
@@ -31,5 +31,38 @@ decoded_image.set_shape([height, width, channels])
 # 定义神经网络输入层图片的大小。
 image_size = 299
 # preprocess_for_train为7.2.2小节中介绍的图像预处理程序
-distorted_image = p182.preprocess_for_train
+distorted_image = p182.preprocess_for_train(
+    decoded_image, image_size, image_size, None
+)
+
+# 将处理后的图像和标签数据通过tf.train.shuffle_batch整理成神经网络训练时
+# 需要的batch
+min_after_dequeque = 10000
+batch_size = 100
+capacity = min_after_dequeque + 3 * batch_size
+image_batch, label_batch = tf.train.shuffle_batch(
+    [distorted_image, label], batch_size=batch_size,
+    capacity=capacity, min_after_dequeue=min_after_dequeque
+)
+
+# 定义神经网络的结构以及优化过程。image_batch可以作为输入提供给神经网络的输入层。
+# label_batch则提供了输入batch中样例的正确答案
+logit = inference(image_batch)
+loss = calc_loss(logit, label_batch)
+train_step = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss)
+
+# 声明会话并运行神经网络的优化过程
+with tf.Session() as sess:
+    # 神经网络训练准备工作。这些工作包括变量初始化、线程启动
+    tf.initialize_all_variables().run()
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+    # 神经网络训练过程
+    for i in range(TRAINING_ROUNDS):
+        sess.run(train_step)
+
+    # 停止所有线程
+    coord.request_stop()
+    coord.join(threads)
 
